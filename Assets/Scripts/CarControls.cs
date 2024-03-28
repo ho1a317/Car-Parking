cf30dc7e3004b6a3e4322d5f45cd76a55c3840dc
ho1a317 <csgofi9@gmail.com>
2024-03-28 22:13:29 +0200

    Control Car XY

--- Assets/Scripts/CarControls.cs
+++ Assets/Scripts/CarControls.cs
@@ -10,6 +10,24 @@ public class CarControls : MonoBehaviour
     public float rotationSpeed = 350f;
     private bool isClik;
 
+    private float curPointX;
+    private float curPointY;
+
+    private enum Direktion
+    {
+        Righr,Left,Top,Bottom,None
+    }
+
+    private Direktion CarDirektionX = Direktion.None;
+    private Direktion CarDirektionY = Direktion.None;
+
+    public enum Axis
+    {
+        Vertical, Horizontal
+    }
+
+    public Axis CarAxis;
+
     [NonSerialized] public Vector3 FinalPositions;
 
     private Rigidbody rb;
@@ -21,6 +39,28 @@ public class CarControls : MonoBehaviour
 
     private void OnMouseDown()
     {
+        curPointX = Input.mousePosition.x;
+        curPointY = Input.mousePosition.y;
+    }
+
+    private void OnMouseUp()
+    {
+        if(Input.mousePosition.x - curPointX > 0)
+        {
+            CarDirektionX = Direktion.Righr;
+        }
+        else
+        {
+            CarDirektionX = Direktion.Left;
+        }
+        if (Input.mousePosition.y - curPointY > 0)
+        {
+            CarDirektionY = Direktion.Top;
+        }
+        else
+        {
+            CarDirektionY = Direktion.Bottom;
+        }
         isClik = true;
     }
 
@@ -29,7 +69,7 @@ public class CarControls : MonoBehaviour
         if (FinalPositions.x != 0)
         {
             transform.position = Vector3.MoveTowards(transform.position, FinalPositions, fainalSpeed * Time.deltaTime);
-            //transform.LookAt(FinalPositions);
+           
             Vector3 lookAtPos = FinalPositions - transform.position;
             lookAtPos.y = 0;
             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(lookAtPos), Time.deltaTime * rotationSpeed);
@@ -45,7 +85,20 @@ public class CarControls : MonoBehaviour
     {
         if (isClik && FinalPositions.x == 0)
         {
-            rb.MovePosition(transform.position + Vector3.forward * speed * Time.fixedDeltaTime);
+            Vector3 whichWay = CarAxis == Axis.Horizontal ? Vector3.forward : Vector3.left;
+
+            speed = Mathf.Abs(speed);
+
+            if (CarDirektionX == Direktion.Left && CarAxis == Axis.Horizontal)
+            {
+                speed *= -1;
+            }
+            else if(CarDirektionY == Direktion.Bottom && CarAxis == Axis.Vertical)
+            {
+                speed *= -1;
+            }
+
+            rb.MovePosition(rb.position + whichWay * speed * Time.fixedDeltaTime);
         }
     }
 }
